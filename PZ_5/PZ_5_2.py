cfadecc1c213f89880ceae8fb92f41562780b4cc
KostrominaVeronika <kostrominaveronika@gmail.com>
2022-11-06 23:43:08 +0300

    Изменила решение практической 5

--- PZ_5/PZ_5_2.py
+++ PZ_5/PZ_5_2.py
@@ -1,13 +1,19 @@
 # Описать функцию выполняющую правый циклический сдвиг
 
-def ShiftRight3(A,B,C):
-    A,B = B,A # Смещение А в В
-    A,C = C,A # Смещение С в А
-    L = [] # пустой список
-    L.append(A) # заполнение списка элементами
-    L.append(B)
-    L.append(C)
-    return L # Заполнение списка
+def ShiftRight3(A,B,C):  # создание функции для правого сдвига
+    A, B = B, A  # Смещение А в В
+    C, A = A, C  # Смещение С в А
+    return A, B, C
 
-
-print(ShiftRight3(22,32,33)) # Вывод функции с значениями
+try:  #
+  A, B, C = 2, 3, 4
+  print('Было A = '+str(A), 'B = '+str(B), 'C = '+str(C))
+  A, B, C = ShiftRight3(A, B, C) # вызов функции с перыми входными данными
+  print('Стало A = '+str(A), 'B = '+str(B), 'C = '+str(C))
+  print('\nВторой заход')
+  A, B, C = 8, 4, 7
+  print('Было A = '+str(A), 'B = '+str(B), 'C = '+str(C))
+  A, B, C = ShiftRight3(A, B, C) # вызов функции со вторыми входными данными
+  print('Стало A = '+str(A), 'B = '+str(B), 'C = '+str(C))
+except TypeError:
+  print('Неверный аргумент')
